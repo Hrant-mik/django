f359019c7f6db9f1c61568d5f5aa1f42b48b35ab
Hrant-mik <hrant.mikaelyan77@gmail.com>
2025-03-01 14:15:16 +0400

    creat a to-do list

---
+++ To-do-list/todolist/main/static/main/js/to_do_list.js
@@ -0,0 +1,41 @@
+const addTaskButton = document.getElementById("add-task-btn");
+const taskInput = document.getElementById("task-input");
+const taskList = document.getElementById("task-list");
+
+
+function addTask() {
+    const taskText = taskInput.value.trim();
+    
+    if (taskText !== "") {
+        const li = document.createElement('li');
+        li.textContent = taskText;
+
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.classList.add("delete-btn");
+        deleteButton.onclick = function() {
+            li.remove();
+        };
+
+        li.appendChild(deleteButton);
+
+    
+        li.onclick = function() {
+            li.classList.toggle("completed");
+        };
+
+        
+        taskList.appendChild(li);
+        
+        
+        taskInput.value = "";
+    }
+}
+
+addTaskButton.addEventListener('click', addTask);
+
+taskInput.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        addTask();
+    }
+});
