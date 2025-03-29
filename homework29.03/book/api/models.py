da4442df0beec77c2cb2f84606320e3e6dd6c801
Hrant-mik <hrant.mikaelyan77@gmail.com>
2025-03-29 15:42:51 +0400

    homework 29.03

---
+++ homework29.03/book/api/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Item(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    published_year = models.IntegerField()
+
+   
